7bb9bdd494e810f89f0f969674606c00d4a88316
Elly H <b.hatooka@gmail.com>
2021-02-19 18:43:13 +1100

    complete project draft

--- index_3c.js
+++ index_3c.js
@@ -36,10 +36,10 @@ app.get('/', (req,res) => {
     
         .then((scheduleTable) => {
         console.log(scheduleTable)
-        res.render('./pages/test', {
-            title: 'Schedule Website', 
+        res.render('./pages/SQL-schedules', {
+            title: 'Schedules List', 
             layout: './pages/layout', 
-            myBestFriends: scheduleTable}
+            scheduleTable: scheduleTable}
         )
     }) 
 
@@ -51,3 +51,78 @@ app.get('/', (req,res) => {
     
 
 })
+
+app.get('/new', (req,res) => {
+    res.render('./pages/SQL-forms', {title: 'New Schedule Form',  layout: './pages/layout'})
+})
+
+app.use(express.urlencoded({ extended: true }))
+
+
+app.post("/new",(req,res)=>{
+    
+    const schedule = {
+        user_name: req.body.user_name,
+        day: req.body.day,
+        start_time: req.body.start_at,
+        end_time: req.body.end_at,
+      }
+    const query = 'INSERT INTO schedule (user_name, day, start_time, end_time) VALUES($1,$2,$3,$4)'
+    database.any(query, [schedule.user_name, schedule.day, schedule.start_time,schedule.end_time]);
+      //res.send(schedule)
+
+      database.any('SELECT * from schedule')
+      //we are about to do a PROMISE
+      
+          .then((scheduleTable) => {
+          console.log(scheduleTable)
+          res.render('./pages/SQL-schedules', {
+              title: 'Schedules List', 
+              layout: './pages/layout', 
+              scheduleTable: scheduleTable}
+          )
+      }) 
+  
+          .catch((err) => {
+             
+              res.render('./pages/error', {title: 'Error',  layout: './pages/layout', err: err})
+          }) 
+      
+    
+    })
+
+
+/*app.post('/new', (req,res) => {
+    const newSchedule = req.body
+    console.log(newSchedule)
+   // database.any(('INSERT INTO schedule (user_name, day, start_time, end_time);')
+    
+   // console.log(newSchedule)
+    res.send(newSchedule)
+})*/
+
+
+
+
+
+
+
+
+
+ /*  database.any('SELECT * from schedule;')
+    //we are about to do a PROMISE
+    
+        .then((scheduleTable) => {
+        console.log(scheduleTable)
+        res.render('./pages/SQL-schedules', {
+            title: 'Schedule Website', 
+            layout: './pages/layout', 
+            scheduleTable: scheduleTable}
+        )
+    }) 
+
+        .catch((err) => {
+           
+            res.render('./pages/error', {title: 'Error',  layout: './pages/layout', err: err})
+        }) */
+    
--- sql/create_schedule_table.sql
+++ sql/create_schedule_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS schedule (
     user_id serial PRIMARY KEY,
     user_name VARCHAR(50) NOT NULL,
     day INT CHECK (day >= 1 AND day <= 7),
-    start_time TIME NOT NULL, 
-    end_time TIME NOT NULL 
+    start_time TIMESTAMP NOT NULL, 
+    end_time TIMESTAMP NOT NULL 
 );
